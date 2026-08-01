94dd181d86f1ef241438f566eea5688cba3d9e80
pavankalyanperla <kalyanpava7578@gmail.com>
2026-08-01 11:49:48 +0530

    hakkerrank

---
+++ HackerRank Questions/Array_DS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Result
+{
+    public static List<int> ReverseArray(List<int> a)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = a.Count - 1; i >= 0; i--)
+        {
+            result.Add(a[i]);
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<int> arr = new List<int>();
+
+        Console.WriteLine("Enter the elements:");
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        List<int> reversed = Result.ReverseArray(arr);
+
+        Console.WriteLine("\nReversed Array:");
+
+        foreach (int num in reversed)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
