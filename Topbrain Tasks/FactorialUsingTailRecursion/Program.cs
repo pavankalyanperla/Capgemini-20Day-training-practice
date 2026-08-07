2819ab9bc250a5aa3ce578f238e753fa99b86b1c
pavankalyanperla <kalyanpava7578@gmail.com>
2026-08-07 12:36:47 +0530

    topbrain task-2 day 8

---
+++ Topbrain Tasks/FactorialUsingTailRecursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"Factorial : {Factorial(number)}");
+
+    }
+
+    static int Factorial(int n, int accumulator = 1)
+    {
+        if (n <= 1)
+        {
+            return accumulator;
+        }
+
+        return Factorial(n-1, accumulator*n);
+    }
+}
